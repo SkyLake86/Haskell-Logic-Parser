b81c6233148d455f247e657d1f609527926a0b95
SkyLake86 <milosz.karpowicz@gmail.com>
2022-04-28 00:15:04 +0200

    Added formula data structure

--- parser.hs
+++ parser.hs
@@ -1,2 +1,15 @@
 
 newtype Parser a = P (String -> [(a, String)])
+
+type Bracket k v = [(k, v)]
+
+find :: Eq k => k -> Bracket k v -> v
+find k ls = head [v | (p, v) <- ls, p == k]
+
+data Formula = Value Bool
+            | Var String
+            | Not Formula
+            | And Formula Formula
+            | Or Formula Formula
+            | Imply Formula Formula
+            | Iff Formula Formula
